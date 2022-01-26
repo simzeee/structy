82d351f0aaab99588e24a9c02a24e408b5499dac
John Sims <jwsims816@gmail.com>
2022-01-25 19:04:05 -0600

    added Alvin's remove_node

---
+++ 20_remove_node/alvin_solution.js
@@ -0,0 +1,27 @@
+// iterative
+
+const removeNode = (head, targetVal) => {
+  if (head.val === targetVal) return head.next;
+  
+  let current = head;
+  let prev = null;
+  while (current !== null) {
+    if (current.val === targetVal) {
+      prev.next = current.next;
+      break;
+    }
+    prev = current;
+    current = current.next;
+  }
+  
+  return head;
+};
+
+// recursive
+
+const removeNode = (head, targetVal) => {
+  if (head === null) return null;
+  if (head.val === targetVal) return head.next;
+  head.next = removeNode(head.next, targetVal);
+  return head;
+};
