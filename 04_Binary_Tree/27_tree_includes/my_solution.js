8c439419ea2dc587bf1d7f10b27ddbfdf9f7b21b
John Sims <jwsims816@gmail.com>
2022-05-20 17:04:54 -0500

    solved tree includes recursively

--- 04_Binary_Tree/27_tree_includes/my_solution.js
+++ 04_Binary_Tree/27_tree_includes/my_solution.js
@@ -20,3 +20,32 @@ const treeIncludes = (root, target) => {
   return false
   
 };
+
+const treeIncludesBreadth = (root, target) => {
+  // breadth first traversal
+  if(root === null){return false}
+  const queue = [root]
+  while(queue.length > 0) {
+    const current = queue.shift();
+    // console.log(current.val)
+    if(current.val === target){
+      return true
+    }
+    if(current.left) queue.push(current.left);
+    if(current.right) queue.push(current.right);
+  }
+  
+  return false
+  
+}
+
+
+const treeIncludesRecursive = (root, target) => {
+   if(root === null){
+    return false
+  }
+  if(root.val === target){
+    return true
+  }
+  return treeIncludes(root.left, target) || treeIncludes(root.right, target)
+}
