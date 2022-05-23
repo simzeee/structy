0e7a57b6498241eedc908c1be1e4baaf594f878b
John Sims <jwsims816@gmail.com>
2022-05-23 17:11:20 -0500

    Python tree_path_finder complete

--- 04_Binary_Tree/30_tree_path_finder/tree_path_finder.py
+++ 04_Binary_Tree/30_tree_path_finder/tree_path_finder.py
@@ -35,8 +35,8 @@ def _path_finder(root, target):
   if root.val == target:
     return [root.val]
   
-  left_path = path_finder_slow(root.left, target)
-  right_path = path_finder_slow(root.right, target)
+  left_path = _path_finder(root.left, target)
+  right_path = _path_finder(root.right, target)
 
   if left_path is not None:
     left_path.append(root.val)
