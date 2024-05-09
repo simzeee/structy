c403f14fffa3eb79eba29b6f2c7a846e68378429
simzeee <jwsims816@gmail.com>
2024-05-09 16:42:09 -0400

    add basic breadth first traversal of binary tree

--- Structy_Python/04_binary_tree/02_breadth_first_values.py
+++ Structy_Python/04_binary_tree/02_breadth_first_values.py
@@ -1,6 +1,26 @@
 from collections import deque
 
 
+def breadth_first_values_basic(root):
+    # use a queue
+    if root is None:
+        return []
+
+    queue = [root]
+    values = []
+
+    while queue:
+        current = queue.pop(0)
+        values.append(current.val)
+
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+
+    return values
+
+
 def breadth_first_values(root):
     if not root:
         return []
