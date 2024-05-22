8f6b009cfb6fe6f5cf3df47340a9bc7c2d0079b9
simzeee <jwsims816@gmail.com>
2024-05-22 16:51:39 -0400

    add append solution to path finder

--- Structy_Python/04_binary_tree/07_path_finder.py
+++ Structy_Python/04_binary_tree/07_path_finder.py
@@ -4,19 +4,51 @@
 #     self.left = None
 #     self.right = None
 
+
 def path_finder(root, target):
-  if root is None:
+    if root is None:
+        return None
+
+    if root.val == target:
+        return [root.val]
+
+    left_path = path_finder(root.left, target)
+    if left_path is not None:
+        return [root.val, *left_path]
+
+    right_path = path_finder(root.right, target)
+    if right_path is not None:
+        return [root.val, *right_path]
+
     return None
 
-  if root.val == target:
-    return [ root.val ]
 
-  left_path = path_finder(root.left, target)
-  if left_path is not None:
-    return [ root.val, *left_path]
+# add append
 
-  right_path = path_finder(root.right, target)
-  if right_path is not None:
-    return [root.val, *right_path]
 
-  return None
+def path_finder_append(root, target):
+    result = _path_finder(root, target)
+    if result is None:
+        return None
+    else:
+        return result[::-1]
+
+
+def _path_finder_append(root, target):
+    if root is None:
+        return None
+
+    if root.val == target:
+        return [root.val]
+
+    left_path = _path_finder_append(root.left, target)
+    if left_path is not None:
+        left_path.append(root.val)
+        return left_path
+
+    right_path = _path_finder_append(root.right, target)
+    if right_path is not None:
+        right_path.append(root.val)
+        return right_path
+
+    return None
