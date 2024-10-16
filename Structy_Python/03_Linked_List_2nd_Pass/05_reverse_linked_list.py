04209a1b17cf53356eb575dd22c6c4e19c02a7c1
simzeee <jwsims816@gmail.com>
2024-10-16 13:25:55 -0400

    add recrusive solution

--- Structy_Python/03_Linked_List_2nd_Pass/05_reverse_linked_list.py
+++ Structy_Python/03_Linked_List_2nd_Pass/05_reverse_linked_list.py
@@ -3,14 +3,23 @@
 #     self.val = val
 #     self.next = None
 
-def reverse_list(head):
-  previous = None
-  current = head
-  
-  while current is not None:
-    next = current.next
-    current.next = previous
-    previous = current
-    current = next
 
-  return previous
+def reverse_list(head):
+    previous = None
+    current = head
+
+    while current is not None:
+        next = current.next
+        current.next = previous
+        previous = current
+        current = next
+
+    return previous
+
+
+def reverse_list(head, prev=None):
+    if head is None:
+        return prev
+    next = head.next
+    head.next = prev
+    return reverse_list(next, head)
